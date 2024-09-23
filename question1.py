5d8846c830f3ab7ce2f87ab4efc325c52afac52b
tedee09 <septedy44@gmail.com>
2024-09-23 22:11:59 +0700

    Update question1.py

--- question1.py
+++ question1.py
@@ -3,10 +3,8 @@ import numpy as np
 def get_DH_table():
     d1, d2, d3 = 0.7, 0.5, 0.3
     
-    dh = np.array([
-        [np.radians(45), d1, 0, np.radians(90)],   
-        [np.radians(0), d2, 0, np.radians(0)],     
-        [np.radians(90), d3, 0, np.radians(0)]     
-    ])
+    dh = np.array([[0, d1, 0, 0],
+                   [0, d2, 0, 0],
+                   [0, d3, 0, 0]])
     
     return dh
