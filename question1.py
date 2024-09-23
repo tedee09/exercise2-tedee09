2363335f9e7a39b128ac6ba673e3ee36fcd7d825
tedee09 <septedy44@gmail.com>
2024-09-23 22:16:54 +0700

    Update question1.py

--- question1.py
+++ question1.py
@@ -3,8 +3,8 @@ import numpy as np
 def get_DH_table():
     d1, d2, d3 = 0.7, 0.5, 0.3
     
-    dh = np.array([[0, d1, 0, 0],
-                   [0, d2, 0, 0],
-                   [0, d3, 0, 0]])
+    dh = np.array([[0, d1, 0, -np.pi/2],  
+                   [0, 0, d2, 0],       
+                   [0, 0, d3, 0]])      
     
     return dh
