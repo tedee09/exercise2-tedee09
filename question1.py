6159ce2200451a359a20ef480a2523ab8f666d97
tedee09 <septedy44@gmail.com>
2024-09-23 23:27:55 +0700

    Update question1.py

--- question1.py
+++ question1.py
@@ -3,8 +3,9 @@ import numpy as np
 def get_DH_table():
     d1, d2, d3 = 0.7, 0.5, 0.3
     
-    dh = np.array([[0, d1, 0, -np.pi/2],  
-                   [0, 0, d2, 0],       
-                   [0, 0, d3, 0]])      
+    # Define DH parameters: [theta, d, a, alpha]
+    dh = np.array([[0, d1, 0, -np.pi/2],  # Joint 1
+                   [0, 0, d2, 0],       # Joint 2
+                   [0, 0, d3, 0]])      # Joint 3
     
     return dh
