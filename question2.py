586643c4ed4d751783f2c3010c8e83b1f93849a1
tedee09 <septedy44@gmail.com>
2024-09-23 23:23:06 +0700

    Update question2.py

--- question2.py
+++ question2.py
@@ -1,9 +1,7 @@
 import numpy as np
 from question1 import get_DH_table
 
-def get_pos():
-    dh_table = get_DH_table()
-    
+def get_pos(dh_table):
     pos = np.zeros(3)
     
     for i in range(dh_table.shape[0]):
@@ -12,9 +10,15 @@ def get_pos():
         a = dh_table[i][2]
         alpha = dh_table[i][3]
         
-        # Calculate position contributions
-        pos[0] += a * np.cos(theta)  # X position contribution from a
-        pos[1] += a * np.sin(theta)  # Y position contribution from a
-        pos[2] += d                  # Z position contribution from d
-
+        # Hitung kontribusi posisi
+        pos[0] += a * np.cos(np.radians(theta))  # Kontribusi X dari a
+        pos[1] += a * np.sin(np.radians(theta))  # Kontribusi Y dari a
+        pos[2] += d                              # Kontribusi Z dari d
+    
     return pos
+
+# Contoh penggunaan
+dh_table = get_DH_table()
+student_pos = get_pos(dh_table)
+
+print("Posisi Akhir Efektor:", student_pos)
